6d6045df43ec51af4015de33f92da546d33c676f
Sleepingoff <just_jh30@naver.com>
2023-04-26 18:04:27 +0900

    button for scroll

--- js/art.js
+++ js/art.js
@@ -1,31 +1,64 @@
 $(document).ready(function(){
+    let clickCount = 1;
+    let subnavChild = $("ul.subnav > li");
     //버튼 클릭 시 양 옆으로 이동하도록 만들기
     //1. 양 옆 버튼 추가하기
     $(".subnavwrap").append("<button class='next'></button>");
     $(".subnavwrap").prepend("<button class='prev'></button>");
     //.subnavwrap안에 ul을 감싸는 div 추가하기
     $("ul.subnav").wrap("<div class='wsubnav'></div>");
+    subnavChild.prepend("<div class=''></div>");
+    //클릭한 대상 표시하기
+   
+    $("ul.subnav > li:first-child div").addClass("bluecircle");
+    subnavChild.click(function(){
+        $(this).children().removeClass("ccc").addClass("blue");
+        $(this).children("div").addClass("bluecircle");
+        $(this).siblings().children("div").removeClass("bluecircle");
+        $(this).siblings().children().addClass("ccc").removeClass("blue");
+        clickCount = $(this).index();
+        console.log(clickCount);
+        return false;
+    });
     //버튼 클릭 이벤트
     let currentPlace = 0;
     //현재 이동된 거리에서 +- 200px을 할 순 없을까
     $(".subnavwrap > button").click(function(){
-        if(currentPlace === 0 && $(this).hasClass("prev")){
-            currentPlace = -1000;//currentPlace = 0 일때 가장 처음/마지막으로 돌아가기
-        }
+        
         if($(this).hasClass("prev")){
-            currentPlace += 200; 
+            clickCount--;
+            
+            if(currentPlace === 200){
+                $("ul.subnav > li:nth-child("+clickCount+")").children().removeClass("ccc").addClass("blue");
+                $("ul.subnav > li:nth-child("+clickCount+")").siblings().children().addClass("ccc").removeClass("blue");
+                $("ul.subnav > li:nth-child("+clickCount+")").children("div").addClass("bluecircle");
+                $("ul.subnav > li:nth-child("+clickCount+")").siblings().children("div").removeClass("bluecircle");
+            } else{
+                $("ul.subnav").css({"transform":"translateX("+ currentPlace +"px)"});
+                currentPlace += 200;
+            }
         }
-        if(currentPlace === -800 && $(this).hasClass("next")){
-            currentPlace = 200;
-        }
+        
         if($(this).hasClass("next")){
-            currentPlace -= 200;
+            clickCount++;
+            if(currentPlace === -800){
+                $("ul.subnav > li:nth-child("+clickCount+")").children().removeClass("ccc").addClass("blue");
+                $("ul.subnav > li:nth-child("+clickCount+")").siblings().children().addClass("ccc").removeClass("blue");
+                $("ul.subnav > li:nth-child("+clickCount+")").children("div").addClass("bluecircle");
+                $("ul.subnav > li:nth-child("+clickCount+")").siblings().children("div").removeClass("bluecircle");
+            }else{
+                currentPlace -= 200;
+                $("ul.subnav").css({"transform":"translateX("+ currentPlace +"px)"});
+            }
             //currentPlace = -800 일때 next가 더 작동되지 않도록 하기
         }
-        $("ul.subnav").css({"transform":"translateX("+ currentPlace +"px)"});
-            console.log(currentPlace);
-        
-        
+        console.log(currentPlace);
+        //clickCurrent를 기준으로 좌우로 왔다갔다하기
+        //currentPlace = -800 || 0일때 clickCurrent next || prev하기
+        $("ul.subnav > li:nth-child("+clickCount+")").children().removeClass("ccc").addClass("blue");
+        $("ul.subnav > li:nth-child("+clickCount+")").siblings().children().addClass("ccc").removeClass("blue");
+        $("ul.subnav > li:nth-child("+clickCount+")").children("div").addClass("bluecircle");
+        $("ul.subnav > li:nth-child("+clickCount+")").siblings().children("div").removeClass("bluecircle");
         //버튼 클릭으로 두 가지를 묶어서 관리하기 > button + prev 두번작동되는 오류
         /*$(".prev").click(function(){
             currentPlace += 200;
@@ -41,15 +74,7 @@ $(document).ready(function(){
         //-
     });
     //ul.subnav 내부 li 클릭시 새로고침되지 않고 색깔 바꾸기
-    $("ul.subnav > li").prepend("<div class=''></div>");
-    $("ul.subnav > li:first-child div").addClass("bluecircle");
-    $("ul.subnav > li").click(function(){
-        $(this).children().removeClass("ccc").addClass("blue");
-        $(this).children("div").addClass("bluecircle");
-        $(this).siblings().children("div").removeClass("bluecircle");
-        $(this).siblings().children().addClass("ccc").removeClass("blue");
-        return false;
-    });
+   
     $("img.more").click(
         function(){
             $(this).parent().siblings("div.hide").fadeIn();
